287a31711647b93dda9862f83251e0e3a757bc75
Asher Nguyen <phucnguyenhoang1985@gmail.com>
2019-08-26 11:51:09 +1000

    Updates

---
+++ precache-manifest.6cf5cf40e4c011be2e4266514120b0d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a711a923876056643b4a4c73861c7569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "883a559f39176253cd9b",
+    "url": "/static/js/2.ab755b92.chunk.js"
+  },
+  {
+    "revision": "09ca15b88161d2ca8b7f",
+    "url": "/static/js/main.fe67fbe8.chunk.js"
+  },
+  {
+    "revision": "6c42619021a7bdc8ad60",
+    "url": "/static/js/runtime~main.d2b6793d.js"
+  }
+]);
